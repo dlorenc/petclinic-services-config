9470b67abb0ea563009ddc12a5738da164cae97d
Phillip Wittrock <pwittroc@google.com>
2019-05-06 14:34:08 -0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ source deploy.env
 gcloud container clusters get-credentials ${DEPLOY_CLUSTER} --zone ${DEPLOY_ZONE}
 
 # Set the plugin string for cli.js
-export KUSTOMIZE_PLUGIN_CONFIG_STRING = $(cat ${SPRING_CLOUD_DIR})
+export KUSTOMIZE_PLUGIN_CONFIG_STRING=$(cat ${SPRING_CLOUD_DIR})
 
 # pipe the kustomized config to cli.js for platform transformations, then apply
 kustomize ${DEPLOY_DIR} | cli.js  | kubectl apply -f -
