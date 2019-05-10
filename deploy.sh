69fcd665f7662d2675c8b6fd6e3311c224d775f5
Phillip Wittrock <pwittroc@google.com>
2019-05-10 11:32:57 -0700

    use cloud-sql and kms for prod

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-source deploy.env
-gcloud container clusters get-credentials ${DEPLOY_CLUSTER} --zone ${DEPLOY_ZONE}
-
-kustomize build --enable_alpha_goplugins_accept_panic_risk ${DEPLOY_DIR} | kubectl apply -f -
